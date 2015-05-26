a1cd5b4a372253effbe7d40a5988fc229e2c81ea
Chris Palmer <palmerc7@att.net>
2015-05-25 20:07:31 -0500

    Updated Xamarin libraries

--- Neudesic.AgileDefender/Neudesic.AgileDefender.Droid/Resources/Resource.Designer.cs
+++ Neudesic.AgileDefender/Neudesic.AgileDefender.Droid/Resources/Resource.Designer.cs
@@ -26,8 +26,8 @@ namespace Neudesic.AgileDefender.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.InsightsCore.Resource.String.ApplicationName = global::Neudesic.AgileDefender.Droid.Resource.String.ApplicationName;
-			global::Xamarin.InsightsCore.Resource.String.Hello = global::Neudesic.AgileDefender.Droid.Resource.String.Hello;
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::Neudesic.AgileDefender.Droid.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::Neudesic.AgileDefender.Droid.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
