ba027b90eadf8e0836ccdb997b9a0fce38cae8e7
Chris Palmer <palmerc7@att.net>
2015-06-06 08:21:54 -0500

    Added ValidationResources helper class

---
+++ AgileDefender/AgileDefender/Helpers/ValidationResources.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileDefender.Helpers
+{
+    public static class ValidationResources
+    {
+        public static string SignInEmptyEmailAddressOrPassword = "Email address and Password are both required";
+    }
+}
